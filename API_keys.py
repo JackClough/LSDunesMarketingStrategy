fedd27c20413af1ea15975deb76928ed114bddd9
JackClough <jackclough18@gmail.com>
2024-07-10 23:33:06 -0600

    Delete API_keys.py

--- API_keys.py
+++
@@ -1,4 +0,0 @@
-api_key = 'mYCuq8PNrGqbgze22gFPVMnWq'
-api_key_secret = 'OFD5nT7ovW3kl2TEQRpruSSCSve3IapgCgVBvCOmE9LhIIEG0f'
-access_token = '977734152-aPkQpYAqSRmB8zhmBzReDkRtg2HOzyvPIJaWCEN1'
-access_token_secret = 'IehSr6vV4dIbqCqCYoegNI3UjHoQyCrwuRjhIZ6YLw02k'
